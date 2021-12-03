eb9df5cfc80f839c4386ce4163b7eeaa91bee16b
Alex Poulsen <lexpoulsen@gmail.com>
2021-12-03 12:10:02 +1100

    gitignore

--- server/app.js
+++ server/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+// HTTP Methods:
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server', app: "Test.js"})
+// });
+//
+// app.post('/', (req, res) => {
+//   res.send('You can post to the endpoint')
+// });
 
 app.listen(port, () => {
   console.log(`App running on port ${ port }...`);
 });
-
-// HTTP Methods:
-
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server', app: "Test.js"})
-});
-
-app.post('/', (req, res) => {
-  res.send('You can post to the endpoint')
-})
