b91f3fe8398305c63dfb5d79940c23f5848fe589
Alex Poulsen <lexpoulsen@gmail.com>
2021-12-03 14:21:38 +1100

    Atlas connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+dotenv.config({ path: './config.env'});
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(`mongodb+srv://user1:${ process.env.DATABASE_PASSWORD }@cluster0.nwx7z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  }).then(con => {
+    console.log(con.connections);
+    console.log('DB Connection successful');
+  })
 
 // HTTP Methods:
 
