35473542f465f20f3afb6d2e2fe6eef7d1458ad7
Alex Poulsen <lexpoulsen@gmail.com>
2021-12-04 18:07:38 +1100

    New user POST working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,34 +1,34 @@
 const User = require('./../models/userModel');
 
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'daddy no'
   })
 };
 
-exports.getUser = (req, res) => {
+exports.getUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined'
   })
 };
 
-exports.createUser = (req, res) => {
+exports.createUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'Create a user'
   })
 };
 
-exports.updateUser = (req, res) => {
+exports.updateUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined'
   })
 };
 
-exports.deleteUser = (req, res) => {
+exports.deleteUser = (req, res, next) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined'
