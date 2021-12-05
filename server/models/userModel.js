a805a29943d5f44035d6c62c57629d98dc7eb864
Alex Poulsen <lexpoulsen@gmail.com>
2021-12-05 15:13:19 +1100

    Backend - implement protected routes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -31,7 +31,8 @@ const userSchema = new mongoose.Schema({
       },
       message: 'Passwords are not the same'
     }
-  }
+  },
+  passwordChangedAt: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -52,6 +53,16 @@ userSchema.methods.correctPassword = async function(
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
+
+userSchema.methods.changedPasswordAfter = function(JWTTimeStamp) {
+
+  if(this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  return false;
+}
 
 const User = mongoose.model('User', userSchema);
 
