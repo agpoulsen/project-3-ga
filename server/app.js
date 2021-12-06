ab49b99385262919b361e36b1ede465b8191d50c
Alex Poulsen <lexpoulsen@gmail.com>
2021-12-06 21:45:01 +1100

    Frontend - Build login and signup components

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./util/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,6 +39,9 @@ app.use(cors());
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb'}));
+
+// Cookie parser, reading data from cookies
+app.use(cookieParser());
 
 // Data Sanitisation against NoSQL query injection
 app.use(mongoSanitize());
